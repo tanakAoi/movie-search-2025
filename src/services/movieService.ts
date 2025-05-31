2d07d651a573a513afa8b28fc07205798efe7a9b
Aoi Tanaka <aoitanaka.0523@gmail.com>
2025-05-31 17:27:49 +0200

    Fix error

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,9 +1,11 @@
+const BASE_URL = process.env.BASE_URL;
+
 export const getPopularMovies = async () => {
-  const response = await fetch("/api/movie/popular");
+  const response = await fetch(`${BASE_URL}/api/movie/popular`);
   return response.json();
 };
 
 export const getMoviesByKeyword = async (query: string) => {
-  const response = await fetch(`/api/movie/search?q=${query}`);
+  const response = await fetch(`${BASE_URL}/api/movie/search?q=${query}`);
   return response.json();
 };
