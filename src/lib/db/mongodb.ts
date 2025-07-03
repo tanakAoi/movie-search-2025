f87f5961298b19e3880c048939bbe762239f2890
Aoi Tanaka <aoitanaka.0523@gmail.com>
2025-07-03 12:31:51 +0200

    test

--- src/lib/db/mongodb.ts
+++ src/lib/db/mongodb.ts
@@ -4,11 +4,6 @@ const uri = process.env.MONGODB_URI!;
 const options = {
   serverSelectionTimeoutMS: 3000,
   autoSelectFamily: false,
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
 };
 
 let client;
@@ -24,7 +19,7 @@ if (!process.env.MONGODB_URI) {
   throw new Error("Please add MONGODB_URI to .env.local");
 }
 
-if (process.env.NODE_ENV === "development") {
+/* if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
@@ -33,6 +28,12 @@ if (process.env.NODE_ENV === "development") {
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
-}
+} */
+
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
 
 export default clientPromise;
