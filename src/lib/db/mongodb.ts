a3fee757a7dadd35a1639d67aaf3848a30d13158
Aoi Tanaka <aoitanaka.0523@gmail.com>
2025-07-03 12:33:09 +0200

    Fix error

--- src/lib/db/mongodb.ts
+++ src/lib/db/mongodb.ts
@@ -1,4 +1,4 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI!;
 const options = {
@@ -18,17 +18,6 @@ declare global {
 if (!process.env.MONGODB_URI) {
   throw new Error("Please add MONGODB_URI to .env.local");
 }
-
-/* if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-} */
 
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
